e122529c380b801cb0d0d7a84a35ad79c538fca6
Capelani <osvaldo.capelani@gmail.com>
2018-08-17 21:18:18 -0400

    correção dos bugs da troca de senha resolvido. Começar o update da senha agora

--- ptrocarSenha.php
+++ ptrocarSenha.php
@@ -4,24 +4,22 @@ if ($_SESSION['nivel'] < 1){
     Você não possui permissão para estar aqui.";
 } else {
         $confereSenhaVelha = $_POST['confereSenhaVelha'];
-        $senhaVelha = $_POST['senhaVelha'];
-        $senha = $_POST['senha'];
-        $senha1 = $_POST['senha1'];
+        $senhaVelha = md5($_POST['senhaVelha']);
+        $senha = md5($_POST['senha']);
+        $senha1 = md5($_POST['senha1']);
 
-        echo $confereSenhaVelha. "<br />";
-        echo md5($senhaVelha). "<br />";
-        echo md5($senha). "<br />";
-        echo md5($senha1). "<br />";
+        //echo $confereSenhaVelha. "<br />";
+        //echo md5($senhaVelha). "<br />";
+       // echo md5($senha). "<br />";
+        //echo md5($senha1). "<br />";
  
 
 
-        if($confereSenhaVelha == $senhaVelha ){
+        if( (strcmp($confereSenhaVelha, $senhaVelha) == 0) && (strcmp($senha, $senha1) == 0 ) ){
             //echo "Senha antiga não confere! <a href='index.php?p=mudarSenha'>Tente novamente.<br /></a>";
             echo "senhas velhas iguais";
+        } else {
+            echo "senha digitadas não conferem! <br />";
         }
-        if($senha == $senha1){
-            echo " Senhas novas são iguais! <a href='index.php?p=mudarSenha'>Tente novamente.</a>";
-        }
-
 
 }//Final do else
