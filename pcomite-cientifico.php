cd5aec1c94761ff8b806bccbb71dc615a8502fe8
Capelani <osvaldo.capelani@gmail.com>
2018-08-24 22:27:24 -0400

    comite da jornada finalizado

--- pcomite-cientifico.php
+++ pcomite-cientifico.php
@@ -1,26 +1,28 @@
 <h4>Comitê científico da IX Jornada de Ensino, Pesquisa e Extensão do IFMT Campus São Vicente</h4>
-Em breve!
-<!--<p>Leandro Carbo - Presidente</p>
-<p><a href="http://lattes.cnpq.br/7333750009942712" target="_blank">Ademir José Conte</a></p>
+
+
+<p><a href="http://lattes.cnpq.br/7333750009942712" target="_blank">Ademir José Conte - Presidente</a></p>
 <p><a href="http://lattes.cnpq.br/9102203889883133" target="_blank">Alexandra de Paiva Soares</a></p>
-<p><a href="http://lattes.cnpq.br/2463053700621319" target="_blank">Anderson Barbieri de Barros</a></p>
 <p><a href="http://lattes.cnpq.br/2264675171868492" target="_blank">Cristiano Martinotto</a></p>
 <p><a href="http://lattes.cnpq.br/6509536264548722" target="_blank">Edione Teixeira de Carvalho</a></p>
-<p><a href="http://lattes.cnpq.br/2265956421611938" target="_blank">Edie Correia Santana</a></p>
-<p><a href="http://lattes.cnpq.br/4216831020096011" target="_blank">Kayena Delaix Zaqueo Queiroz</a></p>
+<p><a href="http://lattes.cnpq.br/7745670578276227" target="_blank">Edson Gomes Evangelista</a></p>
+<p><a href="#" target="_blank">Luiz Carlos da Fonseca Lage</a></p>
 <p><a href="http://lattes.cnpq.br/4213831782865408" target="_blank">Patricia Sobral Silva</a></p>
-<p><a href="http://lattes.cnpq.br/9139475718201089" target="_blank">Ronaldo Eutáquio Feitosa Senra</a></p>
--->
+<p><a href="http://lattes.cnpq.br/7344374835565622" target="_blank">Poliana Fernandes de Almeida</a></p>
+<p><a href="http://lattes.cnpq.br/4879850867215358" target="_blank">Saulo Teixeira de Moura</a></p>
+<p><a href="#" target="_blank">Silvana Alves Pedroso</a></p>
+<p><a href="http://lattes.cnpq.br/1704126984813989" target="_blank">Silvana Angelica Gama Gomes</a></p>
+
 <h4>Comissão de Divulgação, inscrição e certificados da IX Jornada de Ensino, Pesquisa e Extensão do IFMT Campus São Vicente</h4>
-Em breve!
-<!--<p><a href="http://lattes.cnpq.br/2760743276390008" target="_blank">Osvaldo Martins Capelani</a></p>
+
+<p><a href="http://lattes.cnpq.br/2426059231506995" target="_blank">Abimael Antunes Marques</a></p>
 <p><a href="http://lattes.cnpq.br/0920051725878936" target="_blank">Anderson Olympio Umbelino de Lima</a></p>
-<p><a href="http://lattes.cnpq.br/7502856416060907" target="_blank">Washington Luiz Pimentel Alves</a></p>
-<p><a href="http://lattes.cnpq.br/8620045806298095" target="_blank">Kamila Alvares Simões Barata</a></p>
-<p><a href="http://lattes.cnpq.br/8293095663563508" target="_blank">Otto Julio Ahlert Pinno da Silva</a></p>
+<p><a href="#" target="_blank">Ernandes Joel de Queiroz</a></p>
+<p><a href="http://lattes.cnpq.br/7641766715893206" target="_blank">Fernando Ariel Moura Aranha</a></p>
+<p><a href="http://lattes.cnpq.br/8707749624832346" target="_blank">Flávia de Souza Mendes</a></p>
+<p><a href="" target="_blank">Inajara Cristina Soares Ciríaco</a></p>
+<p><a href="" target="_blank">Joice Naiara Soares da Silva</a></p>
+<p><a href="http://lattes.cnpq.br/2760743276390008" target="_blank">Osvaldo Martins Capelani</a></p>
+<p><a href="http://lattes.cnpq.br/9671928593992791" target="_blank">Rennan Magalhães Taques</a></p>
 <p><a href="http://lattes.cnpq.br/7939634399352086" target="_blank">Sérgio Thompson Bernardes Monteiro</a></p>
-<p><a href="http://lattes.cnpq.br/7184799248724631" target="_blank">Paula Fernanda Oliveira</a></p>
-<p><a href="http://lattes.cnpq.br/1334160470525944" target="_blank">Fernanda Martins Dias</a></p>
-<p><a href="http://lattes.cnpq.br/3841623168780319" target="_blank">Evandro Maciel Garcia</a></p>
-<p><a href="http://lattes.cnpq.br/5059442799033981" target="_blank">Átila Milhomem da Costa</a></p>
--->
+<p><a href="http://lattes.cnpq.br/7502856416060907" target="_blank">Washington Luiz Pimentel Alves</a></p>
